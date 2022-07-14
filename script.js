40f68e06844fa1f99aa34ef95f5a0448dea4d848
JosephOkpala <okpalajoseph995@gmail.com>
2022-07-15 00:41:29 +0100

    Committt

--- script.js
+++ script.js
@@ -2,8 +2,3 @@ const hamburger = document.querySelector('.burger');
 const nav = document.querySelector('.navbar-links');
 
 hamburger.addEventListener('click', navActive);
-
-function navActive() {
-  if ((hamburger = aa)) {
-  }
-}
