7574e90916aa084111123320eb1ae97a6c030b78
JosephOkpala <okpalajoseph995@gmail.com>
2022-07-17 00:20:16 +0100

    c

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+const navSlide = () => {
+  const burger = document.querySelector('.burger');
+  const nav = document.querySelector('.navbar-links');
 
+  burger.addEventListener('click', () => {
+    nav.classList.toggle('nav-active');
+
+    burger.classList.toggle('toggle');
+  });
+};
+
+navSlide();
